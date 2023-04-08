7529976160ead91946c976e8f7e724b9f5010b4a
Krzysztof Napiórkowski <krzysna@gmail.com>
2023-04-08 15:54:36 +0200

    Full

---
+++ for_students.py
@@ -0,0 +1,106 @@
+from itertools import compress
+import random
+import time
+import matplotlib.pyplot as plt
+
+from data import *
+
+def initial_population(individual_size, population_size):
+    return [[random.choice([True, False]) for _ in range(individual_size)] for _ in range(population_size)]
+
+def fitness(items, knapsack_max_capacity, individual):
+    total_weight = sum(compress(items['Weight'], individual))
+    if total_weight > knapsack_max_capacity:
+        return 0
+    return sum(compress(items['Value'], individual))
+
+def population_best(items, knapsack_max_capacity, population):
+    best_individual = None
+    best_individual_fitness = -1
+    for individual in population:
+        individual_fitness = fitness(items, knapsack_max_capacity, individual)
+        if individual_fitness > best_individual_fitness:
+            best_individual = individual
+            best_individual_fitness = individual_fitness
+    return best_individual, best_individual_fitness
+
+
+items, knapsack_max_capacity = get_big()
+print(items)
+
+population_size = 100
+generations = 200
+n_selection = 20
+n_elite = 1
+
+start_time = time.time()
+best_solution = None
+best_fitness = 0
+population_history = []
+best_history = []
+population = initial_population(len(items), population_size) # Tworzenie początkowej populacji rowiązań
+for _ in range(generations):
+    population_history.append(population)
+
+    # TODO: implement genetic algorithm
+    fitness_values = []
+    
+    # Wybór rodziców - selekcja ruletkowa
+    for individual in population:
+        fitness_values.append(fitness(items, knapsack_max_capacity, individual))
+    
+    fitness_values = [float(f)/sum(fitness_values) for f in fitness_values]
+    
+    new_population = []
+    for _ in range(int(len(population)/2)):
+
+        parents = random.choices(population, weights=fitness_values, k=2)
+        # Tworzenie kolejnego pokolenia - crossover
+        crossover_point = random.randint(1, len(items) - 1)
+        child1 = parents[0][0:crossover_point] + parents[1][crossover_point:]
+        child2 = parents[1][0:crossover_point] + parents[0][crossover_point:]
+        
+        
+        # Mutacja
+        if( random.randint(1, 10) == 1):
+            mutation_point = random.randint(0, len(items)-1)
+            child1[mutation_point] = not child1[mutation_point]
+        
+        if( random.randint(1, 10) == 1):
+            mutation_point = random.randint(0, len(items)-1)
+            child2[mutation_point] = not child2[mutation_point]
+        
+        #new_population.append(random.choice([child1,child2]))
+        new_population.append(child1)
+        new_population.append(child2)
+        
+    population = new_population
+
+    # Aktualizacja populacji rozwiązań
+    best_individual, best_individual_fitness = population_best(items, knapsack_max_capacity, population)
+    if best_individual_fitness > best_fitness:
+        best_solution = best_individual
+        best_fitness = best_individual_fitness
+    best_history.append(best_fitness)
+
+end_time = time.time()
+total_time = end_time - start_time
+print('Best solution:', list(compress(items['Name'], best_solution)))
+print('Best solution value:', best_fitness)
+print('Time: ', total_time)
+
+# plot generations
+x = []
+y = []
+top_best = 10
+for i, population in enumerate(population_history):
+    plotted_individuals = min(len(population), top_best)
+    x.extend([i] * plotted_individuals)
+    population_fitnesses = [fitness(items, knapsack_max_capacity, individual) for individual in population]
+    population_fitnesses.sort(reverse=True)
+    y.extend(population_fitnesses[:plotted_individuals])
+plt.scatter(x, y, marker='.')
+plt.plot(best_history, 'r')
+plt.xlabel('Generation')
+plt.ylabel('Fitness')
+plt.show()
